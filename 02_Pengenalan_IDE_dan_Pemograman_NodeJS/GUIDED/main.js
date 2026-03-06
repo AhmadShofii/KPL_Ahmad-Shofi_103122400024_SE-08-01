935ebc0a4cedad2333b36a3676b5f468b5ddbc31
ahmadshofii023 <ahmadshofii023@gmail.com>
2026-03-06 18:51:53 +0700

    main

---
+++ 02_Pengenalan_IDE_dan_Pemograman_NodeJS/GUIDED/main.js
@@ -0,0 +1,11 @@
+const kotaDikunjungi = ["Jakarta", "Bandung", "Surabaya", "Yogyakarta", "Medan"];
+
+kotaDikunjungi.push ("Bali");
+kotaDikunjungi.pop();
+kotaDikunjungi.unshift ("Semarang");
+kotaDikunjungi.shift();
+// kotaDikunjungi.reverse(); 
+// kotaDikunjungi.splice(2, 1, "Malang", "Solo");
+
+console.log(kotaDikunjungi);
+// console.log(kotaDikunjungi.slice(1));
